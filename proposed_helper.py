8bd4f0c0cc11c11b54b77f0c54960e3480c3fcb5
Dhruv-mak <dhruv.makwana.169@gmail.com>
2022-01-30 18:17:46 +0530

    Compute R_j

--- proposed_helper.py
+++ proposed_helper.py
@@ -1,6 +1,7 @@
 from re import sub
 import networkx as nx
 import math
+import numpy as np
 def compute_katz(graph):
     G = nx.Graph()
     G.add_nodes_from(nx.path_graph(graph.nodes))
@@ -11,14 +12,31 @@ def compute_katz(graph):
     centrality = nx.katz_centrality(G,1/phi-0.01)
     # for n,c in centrality.items():
     #     print(f"{n} {c}")
-    #   
+    centrality = np.array([centrality[i] for i in range(graph.nodes)])
     return centrality
 
+# subtract the edge weights in the nodes.........
 def compute_strength(graph):  
-    strength = dict()
+    strength = [0 for _ in range(graph.nodes)]
     for u in range(graph.nodes):
-        strength[u] = 0
         for v in graph.neighbours[u]:
             strength[u] += graph.edge_weights[(str(u),str(v))]
-    return strength
+    return np.array(strength)
 
+def get_ranks(graph):
+    # 0: degree
+    # 1: cetrality
+    # 2: strength
+    degree = np.arary([len(graph.neighbours[i]) for i in range(graph.nodes)])
+    centrality = compute_katz(graph)
+    strength = compute_strength(graph)
+    deg_max, deg_min = np.max(degree), np.min(degree)
+    cen_max, cen_min = np.max(centrality), np.min(centrality)
+    str_max, str_min = np.max(strength), np.min(strength)
+    deg_weight = 1/3
+    cen_weight = 1/3
+    str_weight = 1/3
+    R_j = (deg_weight * (deg_max - degree))/(deg_max - deg_min)
+    R_j += (cen_weight * (cen_max - degree))/(cen_max - cen_min)
+    R_j = (str_weight * (str_max - degree))/(str_max - str_min)
+    
