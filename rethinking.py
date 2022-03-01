3851902596c799b3577224f28d93504998187ac8
Jyotirma02 <jyotirmays123@gmail.com>
2022-03-01 23:10:25 +0530

    Add more substrate & vnr details and avg path length

--- rethinking.py
+++ rethinking.py
@@ -8,6 +8,7 @@ from rethinking_helper import *
 
 def main():
     substrate, vne_list = helper.read_pickle()
+    copy_sub = copy.deepcopy(substrate)
     logging.basicConfig(filename="rethinking.log", filemode="w", level=logging.INFO)
     logging.info(f"\n\n\t\t\t\t\t\tSUBSTRATE NETWORK (BEFORE MAPPING VNRs)")
     logging.info(
@@ -44,6 +45,7 @@ def main():
     start_time = datetime.now().time()
     accepted = 0
     revenue = 0
+    path_cnt=0
     curr_map = dict()  # only contains the requests which are successfully mapped
     pre_resource_edgecost = (
         sum(substrate.edge_weights.values()) // 2
@@ -191,6 +193,8 @@ def main():
 
         accepted += 1
         curr_map[req_no] = selected_map
+        for itr in range(len(selected_map.edge_map)):
+            path_cnt += len(selected_map.edge_map[itr])
         revenue += sum(vne_list[req_no].node_weights.values()) + sum(vne_list[req_no].edge_weights.values())//2
         logging.info(f"\n\n")
 
@@ -199,11 +203,23 @@ def main():
     for request in curr_map.values():
         ed_cost += request.edge_cost  # total bandwidth for all the mapped requests
         no_cost += request.node_cost  # total crb for all the mapped requests
-
     tot_cost = ed_cost + no_cost
-    post_resource = (
-        sum(substrate.node_weights.values()) + sum(substrate.edge_weights.values()) // 2
-    )
+
+    post_resource_edgecost =0
+    post_resource_nodecost=0
+    utilized_nodes=0
+    utilized_links=0
+    for edge in substrate.edge_weights:
+        post_resource_edgecost += substrate.edge_weights[edge]
+        if substrate.edge_weights[edge]!=copy_sub.edge_weights[edge]:
+            utilized_links += 1
+    post_resource_edgecost //= 2
+    for node in substrate.node_weights:
+        post_resource_nodecost += substrate.node_weights[node]
+        if substrate.node_weights[node] != copy_sub.node_weights[node]:
+            utilized_nodes += 1
+
+    post_resource = post_resource_edgecost + post_resource_nodecost
 
     end_time = datetime.now().time()
     duration = datetime.combine(date.min, end_time) - datetime.combine(
@@ -222,9 +238,7 @@ def main():
     print(f"Average execution time {duration/len(vne_list)}")
 
     logging.info(f"\n\n\t\t\t\t\t\tSUBSTRATE NETWORK AFTER MAPPING VNRs")
-    logging.info(
-        f"\t\tTotal number of nodes and edges in substrate network is : {substrate.nodes} and {len(substrate.edges)} "
-    )
+    logging.info(f"\t\tTotal number of nodes and edges in substrate network is : {substrate.nodes} and {len(substrate.edges)} ")
     temp = []
     for node in range(substrate.nodes):
         temp.append((node, substrate.node_weights[node]))
@@ -238,25 +252,18 @@ def main():
     if tot_cost == 0:
         logging.error(f"\t\tCouldn't embedd any request")
         return
-
     logging.info(f"\t\tThe revenue to cost ratio is {(revenue/tot_cost)*100:.4f}%")
-    logging.info(
-        f"\t\tTotal number of requests embedded is {accepted} out of {len(vne_list)}"
-    )
-    logging.info(f"\t\tEmbedding ratio is {(accepted/len(vne_list))*100:.4f}%")
-    logging.info(f"\t\tAvailabe substrate resources before mapping is {pre_resource}")
-    logging.info(
-        f"\t\tConsumed substrate resources after mapping is {pre_resource - post_resource}"
-    )
-    logging.info(
-        f"\t\tAverage link utilization {(ed_cost/pre_resource_edgecost)*100:.4f}%"
-    )
-    logging.info(
-        f"\t\tAverage node utilization {(no_cost/pre_resource_nodecost)*100:.4f}%"
-    )
-    logging.info(
-        f"\t\tAverage execution time {duration/len(vne_list)} (HH:MM:SS)\n\n\n"
-    )
+    logging.info(f"\t\tTotal number of requests embedded is {accepted} out of {len(vne_list)}")
+    logging.info(f"\t\tEmbedding ratio is {(accepted/len(vne_list))*100:.4f}%\n")
+    logging.info(f"\t\tTotal {utilized_nodes} nodes are utilized out of {len(substrate.node_weights)}")
+    logging.info(f"\t\tTotal {utilized_links//2} links are utilized out of {len(substrate.edge_weights)//2}\n")
+    logging.info(f"\t\tAvailabe substrate before embedding CRB: {pre_resource_nodecost} BW: {pre_resource_edgecost} total: {pre_resource}")
+    logging.info(f"\t\tAvailabe substrate after embedding CRB: {post_resource_nodecost} BW: {post_resource_edgecost} total: {post_resource}")
+    logging.info(f"\t\tConsumed substrate CRB: {pre_resource_nodecost-post_resource_nodecost} BW: {pre_resource_edgecost-post_resource_edgecost} total: {pre_resource - post_resource}\n")
+    logging.info(f"\t\tAverage Path length is {(path_cnt/accepted):.4f}")
+    logging.info(f"\t\tAverage link utilization {(ed_cost/pre_resource_edgecost)*100:.4f}%")
+    logging.info(f"\t\tAverage node utilization {(no_cost/pre_resource_nodecost)*100:.4f}%")
+    logging.info(f"\t\tAverage execution time {duration/len(vne_list)} (HH:MM:SS)\n\n\n")
     # logging.shutdown()
     output_dict = {
         "revenue": revenue,
